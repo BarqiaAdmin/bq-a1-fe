3472d97913d10a58b250d2f24889380a7ca7c5e5
Juampi Badino <juanpablobadino6@gmail.com>
2023-09-06 08:09:26 -0300

    (Fix commit previo)

--- pages/Usuario/Perfil.tsx
+++ pages/Usuario/Perfil.tsx
@@ -169,7 +169,7 @@ export default function Perfil() {
     };
 
     const handleShare = () => {
-        let shareLink = 'https://https://bq-a1-fe-t8pc.vercel.app/Usuario/' + email
+        let shareLink = 'https://bq-a1-fe-t8pc.vercel.app/Usuario/' + email
         navigator.clipboard.writeText(shareLink);
         toast({
             title: 'Enlace copiado al portapapeles',
