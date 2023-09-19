737f67206fc4d014c1c70069bf255226e5557f6d
Juampi Badino <juanpablobadino6@gmail.com>
2023-09-19 09:01:40 -0300

    Avances varion

--- pages/Sesion/PassRecMailSent.tsx
+++ pages/Sesion/PassRecMailSent.tsx
@@ -24,7 +24,7 @@ const PassRecMailSent = () => {
             method: 'POST',
             headers: {
                 'accept': 'application/json',
-                ''
+                "Content-Type": "application/json"
             }
         })
     }
